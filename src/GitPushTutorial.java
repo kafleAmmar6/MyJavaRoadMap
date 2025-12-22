6679da88044138bb0797baef4dae00faf94445f1
kafleAmmar6 <ammarkafle.edu@gmail.com>
2025-12-22 18:02:46 +0545

    Updated

---
+++ src/GitPushTutorial.java
@@ -0,0 +1,16 @@
+public class GitPushTutorial {
+    public static void main(String[] args) {
+
+
+/*
+        PUSHING ON GITHUB TO NEW REPO WHICH IS RECENT CREATED STEP
+       STEP 1 = giT init
+       STEP 2 = git add .
+       STEP 3 = git commit -m "Initial commit"
+       STEP 4 = git branch -M main
+       STEP 5 = git remote add origin ( REPO LINK )
+       STEP 6 = git push -u origin main
+*/
+        System.out.println("This is GitHub Tutorial");
+    }
+}
